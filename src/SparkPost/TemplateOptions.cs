e70411e06ad89019d7ac19880c1bcdf7fd9d9fb3
Nicholas Mayne <Jetski5822@gmail.com>
2016-07-22 16:28:27 +0100

    Missed the options

--- src/SparkPost/TemplateOptions.cs
+++ src/SparkPost/TemplateOptions.cs
@@ -7,5 +7,6 @@ namespace SparkPost
         public bool? OpenTracking { get; set; }
         public bool? ClickTracking { get; set; }
         public bool? Transactional { get; set; }
+        public bool? Sandbox { get; set; }
     }
 }
